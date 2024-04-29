2aa42eb263186ddb4d9137efbcba2e12e756849d
Gary Smith <garysmith193358@gmail.com>
2024-04-29 00:49:07 -0400

    added tabulation+ solution for fib

--- DynamicProgramming/fib.py
+++ DynamicProgramming/fib.py
@@ -1,10 +1,26 @@
-def f(n): # Tabulation
-    fib = {0:1, 1:1}
+# def f(n): # Tabulation
+#     fib = {0:1, 1:1}
+
+#     for i in range(2, n+1):
+#         fib[i] = fib[i - 1] + fib[i - 2]
+    
+#     return fib[n]
+
+#     # Time O(N)
+#     # Space O(N) 
+
+
+def f(n): # Tabulation+
+
+    prevfib = 1
+    currfib = 1
 
     for i in range(2, n+1):
-        fib[i] = fib[i - 1] + fib[i - 2]
-    
-    return fib[n]
+        temp = currfib
+        currfib = currfib + prevfib
+        prevfib = temp
+        
+    return currfib
 
-    # Time O(N)
-    # Space O(N) 
+    # Time (N)
+    # Space O(1)
